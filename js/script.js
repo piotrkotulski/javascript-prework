62108bcceff743e995b003258911e4032bd8e50c
Piotr Kotulski <piotr.kotulski1986@gmail.com>
2023-07-10 08:56:00 +0200

    added js logic veryfying who won

--- js/script.js
+++ js/script.js
@@ -24,3 +24,39 @@ if(randomNumber == 1){
 
 
 printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+    playerMove = 'kamień';
+}else if (playerInput == '2'){
+    playerMove = 'papier';
+} else if(playerInput == '3'){
+    playerMove = 'nożyce';
+} else{
+    playerMove;
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+}else if( computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('Ja wygrywam!');
+}else if( computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('Ja wygrywam!');
+}else if( computerMove == 'nożyce' && playerMove == 'kamień') {
+    printMessage('Ty wygrywasz!');
+}else if( computerMove == 'papier' && playerMove == 'nożyce') {
+    printMessage('Ja wygrywam!');
+}else if( computerMove == 'kamień' && playerMove == 'nożyce') {
+    printMessage('Ja wygrywam!');
+}else if(computerMove == playerMove){
+    printMessage('Nic z tego mamy remis!');
+}else{
+    printMessage('Graczu pamiętaj wybierz numer z zakresu 1-3!');
+}
